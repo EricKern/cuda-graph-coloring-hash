4953343f4cbb0d3dbefc4464b4a25375774068a8
DanielRe1 <dani.reith@web.de>
2023-01-21 18:16:38 +0100

    Dist2 benchmark & copy kernel

--- benchmark/benchmark.cu
+++ benchmark/benchmark.cu
@@ -5,18 +5,28 @@
 #include "coloringCounters.cuh"
 #include "copyKernel.cuh"
 
+static constexpr const char* Mat = def::Mat3_Cluster;
+static constexpr int STEPS = 300;
+
 using namespace apa22_coloring;
 
+template <bool dist2 = false>
 void initBenchmark(const char* Matrix, int* &d_row_ptr, int* &d_col_ptr, int* &d_tile_boundaries,
-                    SOACounters* &d_soa_total, SOACounters* &d_soa_max, Counters* &d_total,
-                    Counters* &d_max, int &max_nodes, int &max_edges, int &shMem_size_bytes,
-                    int &number_of_tiles, size_t &size){
+                    SOACounters* &d_soa_total, SOACounters* &d_soa_max, SOACounters &h_soa_total,
+                    SOACounters &h_soa_max, Counters* &d_total, Counters* &d_max, int &max_nodes,
+                    int &max_edges, int &shMem_size_bytes, int &number_of_tiles, size_t &size){
     int* row_ptr;
     int* col_ptr;
     double* val_ptr;
     int m_rows;
     int* ndc_;
-    kernel_setup(Matrix, row_ptr, col_ptr, val_ptr, ndc_, m_rows, number_of_tiles, shMem_size_bytes, 300);
+
+    if constexpr (dist2){
+        kernel_setup<true>(Matrix, row_ptr, col_ptr, val_ptr, ndc_, m_rows, number_of_tiles, shMem_size_bytes, STEPS);
+    }
+    else{
+        kernel_setup(Matrix, row_ptr, col_ptr, val_ptr, ndc_, m_rows, number_of_tiles, shMem_size_bytes, STEPS);
+    }
   
     size_t row_ptr_len = m_rows + 1;
     size_t col_ptr_len = size = row_ptr[m_rows];
@@ -35,11 +45,9 @@ void initBenchmark(const char* Matrix, int* &d_row_ptr, int* &d_col_ptr, int* &d
              cudaMemcpyHostToDevice);
 
     // For each bit_width we allocate a counter for each block and for each hash function
-    SOACounters h_soa_total;
     for (int i = 0; i < hash_params.len; ++i) {
         cudaMalloc((void**)&(h_soa_total.m[i]), num_bit_widths * number_of_tiles * sizeof(int));
     }
-    SOACounters h_soa_max;
     for (int i = 0; i < hash_params.len; ++i) {
         cudaMalloc((void**)&(h_soa_max.m[i]), num_bit_widths * number_of_tiles * sizeof(int));
     }
@@ -65,12 +73,35 @@ void initBenchmark(const char* Matrix, int* &d_row_ptr, int* &d_col_ptr, int* &d
 	delete[] ndc_;
 }
 
-void copyBench(nvbench::state &state){
+void initBenchmarkD2(SOACounters &h_soa_total2, SOACounters &h_soa_max2, SOACounters* &d_soa_total2,
+                    SOACounters* &d_soa_max2, Counters* &d_total2, Counters* &d_max2, int number_of_tiles){
+    for (int i = 0; i < hash_params.len; ++i) {
+      cudaMalloc((void**)&(h_soa_total2.m[i]), num_bit_widths * number_of_tiles * sizeof(int));
+    }
+    for (int i = 0; i < hash_params.len; ++i) {
+      cudaMalloc((void**)&(h_soa_max2.m[i]), num_bit_widths * number_of_tiles * sizeof(int));
+    }
+
+    cudaMalloc((void**)&d_soa_total2, sizeof(SOACounters));
+    cudaMalloc((void**)&d_soa_max2, sizeof(SOACounters));
+
+    cudaMemcpy(d_soa_total2, &h_soa_total2, sizeof(SOACounters),
+              cudaMemcpyHostToDevice);
+    cudaMemcpy(d_soa_max2, &h_soa_max2, sizeof(SOACounters),
+              cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&d_total2, hash_params.len * sizeof(Counters));
+    cudaMalloc((void**)&d_max2, hash_params.len * sizeof(Counters));
+}
+
+void copyBenchDist1(nvbench::state &state){
     int* d_row_ptr;
     int* d_col_ptr;
     int* d_tile_boundaries;
     SOACounters* d_soa_total;
     SOACounters* d_soa_max;
+    SOACounters h_soa_total;
+    SOACounters h_soa_max;
     Counters* d_total;
     Counters* d_max;
     int shMem_size_bytes;
@@ -80,8 +111,9 @@ void copyBench(nvbench::state &state){
     size_t size;
 
     state.set_timeout(-1);
-    initBenchmark(def::Mat3, d_row_ptr, d_col_ptr, d_tile_boundaries, d_soa_total, d_soa_max,
-                d_total, d_max, max_nodes, max_edges, shMem_size_bytes, number_of_tiles, size);
+    initBenchmark(Mat, d_row_ptr, d_col_ptr, d_tile_boundaries, d_soa_total, d_soa_max,
+                h_soa_total, h_soa_max, d_total, d_max, max_nodes, max_edges, shMem_size_bytes,
+                number_of_tiles, size);
     
     state.add_element_count(size, "Elements");
     state.add_global_memory_reads<int>(size);
@@ -96,11 +128,15 @@ void copyBench(nvbench::state &state){
     dim3 blockSize(THREADS);
 
     state.exec([&](nvbench::launch &launch){
-        copyKernel<<<gridSize, blockSize, shMem_size_bytes, launch.get_stream()>>>(
+        copyKernelDist1<<<gridSize, blockSize, shMem_size_bytes, launch.get_stream()>>>(
             d_row_ptr, d_col_ptr, d_tile_boundaries, max_nodes, max_edges, d_soa_total,
             d_soa_max, d_total, d_max);
     });
 
+    for (int i = 0; i < hash_params.len; ++i) {
+        cudaFree(h_soa_total.m[i]);
+        cudaFree(h_soa_max.m[i]);
+    }
     cudaFree(d_row_ptr);
     cudaFree(d_col_ptr);
     cudaFree(d_tile_boundaries);
@@ -116,6 +152,8 @@ void coloring1Bench(nvbench::state &state){
     int* d_tile_boundaries;
     SOACounters* d_soa_total;
     SOACounters* d_soa_max;
+    SOACounters h_soa_total;
+    SOACounters h_soa_max;
     Counters* d_total;
     Counters* d_max;
     int shMem_size_bytes;
@@ -125,8 +163,9 @@ void coloring1Bench(nvbench::state &state){
     size_t size;
 
     state.set_timeout(-1);
-    initBenchmark(def::Mat3, d_row_ptr, d_col_ptr, d_tile_boundaries, d_soa_total, d_soa_max,
-                d_total, d_max, max_nodes, max_edges, shMem_size_bytes, number_of_tiles, size);
+    initBenchmark(Mat, d_row_ptr, d_col_ptr, d_tile_boundaries, d_soa_total, d_soa_max,
+                h_soa_total, h_soa_max, d_total, d_max, max_nodes, max_edges, shMem_size_bytes,
+                number_of_tiles, size);
     
     state.add_element_count(size, "Elements");
     state.add_global_memory_reads<int>(size);
@@ -146,6 +185,10 @@ void coloring1Bench(nvbench::state &state){
             d_soa_max, d_total, d_max);
     });
 
+    for (int i = 0; i < hash_params.len; ++i) {
+        cudaFree(h_soa_total.m[i]);
+        cudaFree(h_soa_max.m[i]);
+    }
     cudaFree(d_row_ptr);
     cudaFree(d_col_ptr);
     cudaFree(d_tile_boundaries);
@@ -155,6 +198,140 @@ void coloring1Bench(nvbench::state &state){
     cudaFree(d_max);
 }
 
+void copyBenchDist2(nvbench::state &state){
+    int* d_row_ptr;
+    int* d_col_ptr;
+    int* d_tile_boundaries;
+    SOACounters* d_soa_total1;
+    SOACounters* d_soa_max1;
+    SOACounters* d_soa_total2;
+    SOACounters* d_soa_max2;
+    SOACounters h_soa_total1;
+    SOACounters h_soa_max1;
+    SOACounters h_soa_total2;
+    SOACounters h_soa_max2;
+    Counters* d_total1;
+    Counters* d_max1;
+    Counters* d_total2;
+    Counters* d_max2;
+    int shMem_size_bytes;
+    int number_of_tiles;
+    int max_nodes;
+    int max_edges;
+    size_t size;
 
-NVBENCH_BENCH(copyBench);
+    state.set_timeout(-1);
+    initBenchmark<true>(Mat, d_row_ptr, d_col_ptr, d_tile_boundaries, d_soa_total1, d_soa_max1,
+                h_soa_total1, h_soa_max1, d_total1, d_max1, max_nodes, max_edges, shMem_size_bytes,
+                number_of_tiles, size);
+    initBenchmarkD2(h_soa_total2, h_soa_max2, d_soa_total2, d_soa_max2, d_total2, d_max2,
+                    number_of_tiles);
+    
+    state.add_element_count(size, "Elements");
+    state.add_global_memory_reads<int>(size);
+    //state.add_global_memory_writes<Counters>(size);
+    state.collect_dram_throughput();
+    state.collect_l1_hit_rates();
+    state.collect_l2_hit_rates();
+    state.collect_loads_efficiency();
+    state.collect_stores_efficiency();
+
+    dim3 gridSize(number_of_tiles);
+    dim3 blockSize(THREADS);
+
+    state.exec([&](nvbench::launch &launch){
+        copyKernelDist2<<<gridSize, blockSize, shMem_size_bytes, launch.get_stream()>>>(d_row_ptr,
+                        d_col_ptr, d_tile_boundaries, max_nodes, max_edges, d_soa_total1, d_soa_max1,
+                        d_soa_total2, d_soa_max2, d_total1, d_max1, d_total2, d_max2);
+    });
+
+    for (int i = 0; i < hash_params.len; ++i) {
+        cudaFree(h_soa_total1.m[i]);
+        cudaFree(h_soa_max1.m[i]);
+        cudaFree(h_soa_total2.m[i]);
+        cudaFree(h_soa_max2.m[i]);
+    }
+    cudaFree(d_row_ptr);
+    cudaFree(d_col_ptr);
+    cudaFree(d_tile_boundaries);
+    cudaFree(d_soa_total1);
+    cudaFree(d_soa_total2);
+    cudaFree(d_soa_max1);
+    cudaFree(d_soa_max2);
+    cudaFree(d_total1);
+    cudaFree(d_total2);
+    cudaFree(d_max1);
+    cudaFree(d_max2);
+}
+
+void coloring2Bench(nvbench::state &state){
+    int* d_row_ptr;
+    int* d_col_ptr;
+    int* d_tile_boundaries;
+    SOACounters* d_soa_total1;
+    SOACounters* d_soa_max1;
+    SOACounters* d_soa_total2;
+    SOACounters* d_soa_max2;
+    SOACounters h_soa_total1;
+    SOACounters h_soa_max1;
+    SOACounters h_soa_total2;
+    SOACounters h_soa_max2;
+    Counters* d_total1;
+    Counters* d_max1;
+    Counters* d_total2;
+    Counters* d_max2;
+    int shMem_size_bytes;
+    int number_of_tiles;
+    int max_nodes;
+    int max_edges;
+    size_t size;
+
+    state.set_timeout(-1);
+    initBenchmark<true>(Mat, d_row_ptr, d_col_ptr, d_tile_boundaries, d_soa_total1, d_soa_max1,
+                h_soa_total1, h_soa_max1, d_total1, d_max1, max_nodes, max_edges, shMem_size_bytes,
+                number_of_tiles, size);
+    initBenchmarkD2(h_soa_total2, h_soa_max2, d_soa_total2, d_soa_max2, d_total2, d_max2,
+                    number_of_tiles);
+    
+    state.add_element_count(size, "Elements");
+    state.add_global_memory_reads<int>(size);
+    //state.add_global_memory_writes<Counters>(size);
+    state.collect_dram_throughput();
+    state.collect_l1_hit_rates();
+    state.collect_l2_hit_rates();
+    state.collect_loads_efficiency();
+    state.collect_stores_efficiency();
+
+    dim3 gridSize(number_of_tiles);
+    dim3 blockSize(THREADS);
+
+    state.exec([&](nvbench::launch &launch){
+        coloring2Kernel<<<gridSize, blockSize, shMem_size_bytes, launch.get_stream()>>>(d_row_ptr,
+                        d_col_ptr, d_tile_boundaries, max_nodes, max_edges, d_soa_total1, d_soa_max1,
+                        d_soa_total2, d_soa_max2, d_total1, d_max1, d_total2, d_max2);
+    });
+
+    for (int i = 0; i < hash_params.len; ++i) {
+        cudaFree(h_soa_total1.m[i]);
+        cudaFree(h_soa_max1.m[i]);
+        cudaFree(h_soa_total2.m[i]);
+        cudaFree(h_soa_max2.m[i]);
+    }
+    cudaFree(d_row_ptr);
+    cudaFree(d_col_ptr);
+    cudaFree(d_tile_boundaries);
+    cudaFree(d_soa_total1);
+    cudaFree(d_soa_total2);
+    cudaFree(d_soa_max1);
+    cudaFree(d_soa_max2);
+    cudaFree(d_total1);
+    cudaFree(d_total2);
+    cudaFree(d_max1);
+    cudaFree(d_max2);
+}
+
+
+NVBENCH_BENCH(copyBenchDist1);
 NVBENCH_BENCH(coloring1Bench);
+NVBENCH_BENCH(copyBenchDist2);
+NVBENCH_BENCH(coloring2Bench);
--- include/copyKernel.cuh
+++ include/copyKernel.cuh
@@ -6,15 +6,15 @@ using namespace apa22_coloring;
 
 template <typename IndexT>
 __global__
-void copyKernel(IndexT* row_ptr,  // global mem
-                IndexT* col_ptr,  // global mem
-                IndexT* tile_boundaries,
-                int tile_max_nodes,
-                int tile_max_edges,
-                SOACounters* soa_total,
-                SOACounters* soa_max,
-                Counters* d_total,
-                Counters* d_max) {
+void copyKernelDist1(IndexT* row_ptr,  // global mem
+                    IndexT* col_ptr,  // global mem
+                    IndexT* tile_boundaries,
+                    int tile_max_nodes,
+                    int tile_max_edges,
+                    SOACounters* soa_total,
+                    SOACounters* soa_max,
+                    Counters* d_total,
+                    Counters* d_max) {
                     
     const IndexT partNr = blockIdx.x;
 
@@ -39,8 +39,13 @@ void copyKernel(IndexT* row_ptr,  // global mem
         // total collisions
         for (int k = 0; k < hash_params.len; k++){
             for (int i = 0; i < num_bit_widths; i++){
+                int* start_addr = soa_total->m[k] + i * gridDim.x;
+                int val = 0;
+                for (int j = threadIdx.x; j < gridDim.x; j+=blockDim.x){
+                    val = start_addr[i];
+                }
                 if (threadIdx.x == 0){
-                    d_total[k].m[i] = i + k;
+                    d_total[k].m[i] = val;
                 }
             }
         }
@@ -48,8 +53,103 @@ void copyKernel(IndexT* row_ptr,  // global mem
         // max collisions
         for (int k = 0; k < hash_params.len; k++){
             for (int i = 0; i < num_bit_widths; i++){
+                int* start_addr = soa_total->m[k] + i * gridDim.x;
+                int val = 0;
+                for (int j = threadIdx.x; j < gridDim.x; j+=blockDim.x){
+                    val = start_addr[i];
+                }
                 if (threadIdx.x == 0){
-                    d_max[k].m[i] = i + k;
+                    d_max[k].m[i] = val;
+                }
+            }
+        }
+    }
+}
+
+template <typename IndexT>
+__global__
+void copyKernelDist2(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int tile_max_nodes,
+                     int tile_max_edges,
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+    extern __shared__ IndexT shMem[];
+    IndexT* shMemRows = shMem;                        
+    IndexT* shMemCols = &shMemRows[tile_max_nodes+1]; 
+    IndexT* shMemWorkspace = &shMemCols[tile_max_edges]; 
+  
+    Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+
+    const IndexT partNr = blockIdx.x;  
+
+    for (int k = 0; k < hash_params.len; k++){
+        for (int i = 0; i < num_bit_widths; i++){
+            if (threadIdx.x == 0){
+                soa_total1->m[k][partNr + i * gridDim.x] = i + k;
+                soa_max1->m[k][partNr + i * gridDim.x] = i - k;
+                soa_total2->m[k][partNr + i * gridDim.x] = i + k;
+                soa_max2->m[k][partNr + i * gridDim.x] = i - k;
+            }
+        }
+    }
+
+    if (blockIdx.x == 0) {
+        for (int k = 0; k < hash_params.len; k++){
+            for (int i = 0; i < num_bit_widths; i++){
+                int* start_addr = soa_total1->m[k] + i * gridDim.x;
+                int val = 0;
+                for (int j = threadIdx.x; j < gridDim.x; j+=blockDim.x){
+                    val = start_addr[i];
+                }              
+                if (threadIdx.x == 0){
+                    d_total1[k].m[i] = val;
+                }
+            }
+        }
+
+        for (int k = 0; k < hash_params.len; k++){
+            for (int i = 0; i < num_bit_widths; i++){
+                int* start_addr = soa_max1->m[k] + i * gridDim.x;
+                int val = 0;
+                for (int j = threadIdx.x; j < gridDim.x; j+=blockDim.x){
+                    val = start_addr[i];
+                }
+                if (threadIdx.x == 0){
+                    d_max1[k].m[i] = val;
+                }
+            }
+        }
+        
+        for (int k = 0; k < hash_params.len; k++){
+            for (int i = 0; i < num_bit_widths; i++){
+                int* start_addr = soa_max2->m[k] + i * gridDim.x;
+                int val = 0;
+                for (int j = threadIdx.x; j < gridDim.x; j+=blockDim.x){
+                    val = start_addr[i];
+                }
+                if (threadIdx.x == 0){
+                    d_max2[k].m[i] = val;
+                }
+            }
+        }
+
+        for (int k = 0; k < hash_params.len; k++){
+            for (int i = 0; i < num_bit_widths; i++){
+                int* start_addr = soa_total2->m[k] + i * gridDim.x;
+                int val = 0;
+                for (int j = threadIdx.x; j < gridDim.x; j+=blockDim.x){
+                    val = start_addr[i];
+                }
+                if (threadIdx.x == 0){
+                    d_total2[k].m[i] = val;
                 }
             }
         }
