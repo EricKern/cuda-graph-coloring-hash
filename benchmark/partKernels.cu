97e6133e783bebe4dd57c9125e381e0e84717f6b
DanielRe1 <dani.reith@web.de>
2023-01-30 18:26:47 +0100

    updated partKernels & benchmarks

--- benchmark/partKernels.cu
+++ benchmark/partKernels.cu
@@ -194,12 +194,273 @@ void D1Normal(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_S
   });
 }
 
+template <int BLK_SM>
+void D2OnlyLoad(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2OnlyLoad<int, THREADS, BLK_SM>;
 
-using MyEnumList = nvbench::enum_type_list<1, 2, 4, 8>;
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+template <int BLK_SM>
+void D2OnlyHashD1(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2OnlyHashD1<int, THREADS, BLK_SM>;
+
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+
+template <int BLK_SM>
+void D2OnlyHashD2(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2OnlyHashD2<int, THREADS, BLK_SM>;
+
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+
+template <int BLK_SM>
+void D2OnlyHash(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2OnlyHash<int, THREADS, BLK_SM>;
+
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+
+template <int BLK_SM>
+void D2HashWrite(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2HashWrite<int, THREADS, BLK_SM>;
+
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+template <int BLK_SM>
+void D2FirstRed(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2FirstRed<int, THREADS, BLK_SM>;
+
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+template <int BLK_SM>
+void D2Normal(nvbench::state &state, nvbench::type_list<nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = coloring2Kernel<int, THREADS, BLK_SM>;
+
+  MatLoader& mat_loader = MatLoader::getInstance(Mat);
+  Tiling tiling(D2, BLK_SM,
+              mat_loader.row_ptr,
+              mat_loader.m_rows,
+              reinterpret_cast<void*>(kernel));
+  GPUSetupD2 gpu_setup(mat_loader.row_ptr,
+                       mat_loader.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.calc_shMem();
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+            gpu_setup.d_row_ptr,
+            gpu_setup.d_col_ptr,
+            gpu_setup.d_tile_boundaries,
+            tiling.tile_target_mem,
+            gpu_setup.d_soa_total1,
+            gpu_setup.d_soa_max1,
+            gpu_setup.d_soa_total2,
+            gpu_setup.d_soa_max2,
+            gpu_setup.d_total1,
+            gpu_setup.d_max1,
+            gpu_setup.d_total2,
+            gpu_setup.d_max2);
+  });
+}
+
+
+using MyEnumList = nvbench::enum_type_list<1, 2, 4>;
 
 NVBENCH_BENCH_TYPES(D1OnlyLoad, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
 NVBENCH_BENCH_TYPES(D1OnlyHash, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
 NVBENCH_BENCH_TYPES(D1HashWrite, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
 NVBENCH_BENCH_TYPES(D1FirstRed, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
-NVBENCH_BENCH_TYPES(D1FirstRed2, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
 NVBENCH_BENCH_TYPES(D1Normal, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2OnlyLoad, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2OnlyHashD1, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2OnlyHashD2, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2OnlyHash, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2HashWrite, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2FirstRed, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
+NVBENCH_BENCH_TYPES(D2Normal, NVBENCH_TYPE_AXES(MyEnumList)).set_type_axes_names({"BLK_SM"});
--- include/partKernels.cuh
+++ include/partKernels.cuh
@@ -164,55 +164,279 @@ void coloring1FirstReduce(IndexT* row_ptr,
   }
 }
 
-// We rely on guarantees of tiling i.e. constant tile size
-// (n_rows + n_cols + 1) = constant, NumTiles = gridDim.x
 template <typename IndexT,
           int THREADS,
           int BLK_SM,
           cub::BlockReduceAlgorithm RED_ALGO=cub::BLOCK_REDUCE_WARP_REDUCTIONS>
 __global__
 __launch_bounds__(THREADS, BLK_SM)
-void coloring1First2T(IndexT* row_ptr,
-                     IndexT* col_ptr,
-                     IndexT* tile_boundaries,   // gridDim.x + 1 elements
-                     SOACounters* soa_total,
-                     SOACounters* soa_max,
-                     Counters* d_total,
-                     Counters* d_max) {
+void coloring2OnlyLoad(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int dyn_shmem_bytes,
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+
   const int partNr = blockIdx.x;
   const int n_tileNodes = tile_boundaries[partNr+1] - tile_boundaries[partNr];
+  const int n_tileEdges = dyn_shmem_bytes/sizeof(IndexT) - (n_tileNodes+1);
 
   extern __shared__ IndexT shMem[];
-  IndexT* shMemRows = shMem;                      // n_tileNodes +1 elements
-  IndexT* shMemCols = &shMemRows[n_tileNodes+1];
+  IndexT* shMemRows = shMem;                        // n_tileNodes + 1 elements
+  IndexT* shMemCols = &shMemRows[n_tileNodes+1];    // n_tileEdges elements
+  IndexT* shMemWorkspace = &shMemCols[n_tileEdges]; // n_tileNodes + n_tileEdges + 1 elements
   
   Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+}
+
+
+template <typename IndexT,
+          int THREADS,
+          int BLK_SM,
+          cub::BlockReduceAlgorithm RED_ALGO=cub::BLOCK_REDUCE_WARP_REDUCTIONS>
+__global__
+__launch_bounds__(THREADS, BLK_SM)
+void coloring2OnlyHashD1(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int dyn_shmem_bytes,     // n_rows + 1 + n_cols
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+  const int partNr = blockIdx.x;
+  const int n_tileNodes = tile_boundaries[partNr+1] - tile_boundaries[partNr];
+  const int n_tileEdges = dyn_shmem_bytes/sizeof(IndexT) - (n_tileNodes+1);
+
+  extern __shared__ IndexT shMem[];
+  IndexT* shMemRows = shMem;                        // n_tileNodes + 1 elements
+  IndexT* shMemCols = &shMemRows[n_tileNodes+1];    // n_tileEdges elements
+  IndexT* shMemWorkspace = &shMemCols[n_tileEdges]; // n_tileNodes + n_tileEdges + 1 elements
   
-  typedef cub::BlockReduce<int, THREADS, RED_ALGO> BlockReduceT;
-  __shared__ typename BlockReduceT::TempStorage temp_storageSum;
-  __shared__ typename BlockReduceT::TempStorage temp_storageMax;
+  Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+
 
   for (int k = 0; k < hash_params.len; ++k) {
-    // thread local array to count collisions
-    Counters total_collisions;
-    Counters max_collisions;
+    Counters total1, max1;
     D1CollisionsLocal(shMemRows, shMemCols, tile_boundaries, hash_params.val[k],
-                      total_collisions, max_collisions);
+                      total1, max1);
+  }
+}
 
+template <typename IndexT,
+          int THREADS,
+          int BLK_SM,
+          cub::BlockReduceAlgorithm RED_ALGO=cub::BLOCK_REDUCE_WARP_REDUCTIONS>
+__global__
+__launch_bounds__(THREADS, BLK_SM)
+void coloring2OnlyHashD2(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int dyn_shmem_bytes,     // n_rows + 1 + n_cols
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+  const int partNr = blockIdx.x;
+  const int n_tileNodes = tile_boundaries[partNr+1] - tile_boundaries[partNr];
+  const int n_tileEdges = dyn_shmem_bytes/sizeof(IndexT) - (n_tileNodes+1);
+
+  extern __shared__ IndexT shMem[];
+  IndexT* shMemRows = shMem;                        // n_tileNodes + 1 elements
+  IndexT* shMemCols = &shMemRows[n_tileNodes+1];    // n_tileEdges elements
+  IndexT* shMemWorkspace = &shMemCols[n_tileEdges]; // n_tileNodes + n_tileEdges + 1 elements
+  
+  Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+  typedef cub::BlockReduce<int, THREADS, RED_ALGO> BlockReduceT;
+
+  __shared__ typename BlockReduceT::TempStorage temp_storage;
+
+
+  for (int k = 0; k < hash_params.len; ++k) {
+    Counters total2, max2;
+    D2CollisionsLocal(shMemRows, shMemCols, shMemWorkspace, tile_boundaries,
+                      hash_params.val[k], total2, max2);
+  }
+}
+
+template <typename IndexT,
+          int THREADS,
+          int BLK_SM,
+          cub::BlockReduceAlgorithm RED_ALGO=cub::BLOCK_REDUCE_WARP_REDUCTIONS>
+__global__
+__launch_bounds__(THREADS, BLK_SM)
+void coloring2OnlyHash(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int dyn_shmem_bytes,     // n_rows + 1 + n_cols
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+  const int partNr = blockIdx.x;
+  const int n_tileNodes = tile_boundaries[partNr+1] - tile_boundaries[partNr];
+  const int n_tileEdges = dyn_shmem_bytes/sizeof(IndexT) - (n_tileNodes+1);
+
+  extern __shared__ IndexT shMem[];
+  IndexT* shMemRows = shMem;                        // n_tileNodes + 1 elements
+  IndexT* shMemCols = &shMemRows[n_tileNodes+1];    // n_tileEdges elements
+  IndexT* shMemWorkspace = &shMemCols[n_tileEdges]; // n_tileNodes + n_tileEdges + 1 elements
+  
+  Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+  typedef cub::BlockReduce<int, THREADS, RED_ALGO> BlockReduceT;
+
+  __shared__ typename BlockReduceT::TempStorage temp_storage;
+
+
+  for (int k = 0; k < hash_params.len; ++k) {
+    Counters total1, max1;
+    Counters total2, max2;
+    D1CollisionsLocal(shMemRows, shMemCols, tile_boundaries, hash_params.val[k],
+                      total1, max1);
+    D2CollisionsLocal(shMemRows, shMemCols, shMemWorkspace, tile_boundaries,
+                      hash_params.val[k], total2, max2);
+  }
+}
+
+
+template <typename IndexT,
+          int THREADS,
+          int BLK_SM,
+          cub::BlockReduceAlgorithm RED_ALGO=cub::BLOCK_REDUCE_WARP_REDUCTIONS>
+__global__
+__launch_bounds__(THREADS, BLK_SM)
+void coloring2HashWrite(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int dyn_shmem_bytes,     // n_rows + 1 + n_cols
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+  const int partNr = blockIdx.x;
+  const int n_tileNodes = tile_boundaries[partNr+1] - tile_boundaries[partNr];
+  const int n_tileEdges = dyn_shmem_bytes/sizeof(IndexT) - (n_tileNodes+1);
+
+  extern __shared__ IndexT shMem[];
+  IndexT* shMemRows = shMem;                        // n_tileNodes + 1 elements
+  IndexT* shMemCols = &shMemRows[n_tileNodes+1];    // n_tileEdges elements
+  IndexT* shMemWorkspace = &shMemCols[n_tileEdges]; // n_tileNodes + n_tileEdges + 1 elements
+  
+  Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+  typedef cub::BlockReduce<int, THREADS, RED_ALGO> BlockReduceT;
+
+  __shared__ typename BlockReduceT::TempStorage temp_storage;
+
+
+  for (int k = 0; k < hash_params.len; ++k) {
+    Counters total1, max1;
+    Counters total2, max2;
+    D1CollisionsLocal(shMemRows, shMemCols, tile_boundaries, hash_params.val[k],
+                      total1, max1);
+    D2CollisionsLocal(shMemRows, shMemCols, shMemWorkspace, tile_boundaries,
+                      hash_params.val[k], total2, max2);
     for (int i = 0; i < num_bit_widths; ++i) {
-      int l1_sum = BlockReduceT(temp_storageSum)
-                       .Reduce(total_collisions.m[i], cub::Sum{}, n_tileNodes);
-      int l1_max = BlockReduceT(temp_storageMax)
-                       .Reduce(max_collisions.m[i], cub::Max{}, n_tileNodes);
+      // soa has an int array for each hash function.
+      // In this array all block reduce results of first counter are stored
+      // contiguous followed by the reduce results of the next counter ...
+      if(threadIdx.x == 0){
+        soa_total1->m[k][partNr + i * gridDim.x] = k + i;
+        soa_max1->m[k][partNr + i * gridDim.x] = i - k;
+        soa_total2->m[k][partNr + i * gridDim.x] = k - i;
+        soa_max2->m[k][partNr + i * gridDim.x] = i * k;
+      }
+    }
+  }
+}
+
+template <typename IndexT,
+          int THREADS,
+          int BLK_SM,
+          cub::BlockReduceAlgorithm RED_ALGO=cub::BLOCK_REDUCE_WARP_REDUCTIONS>
+__global__
+__launch_bounds__(THREADS, BLK_SM)
+void coloring2FirstRed(IndexT* row_ptr,  // global mem
+                     IndexT* col_ptr,  // global mem
+                     IndexT* tile_boundaries,
+                     int dyn_shmem_bytes,     // n_rows + 1 + n_cols
+                     SOACounters* soa_total1,
+                     SOACounters* soa_max1,
+                     SOACounters* soa_total2,
+                     SOACounters* soa_max2,
+                     Counters* d_total1,
+                     Counters* d_max1,
+                     Counters* d_total2,
+                     Counters* d_max2) {
+  const int partNr = blockIdx.x;
+  const int n_tileNodes = tile_boundaries[partNr+1] - tile_boundaries[partNr];
+  const int n_tileEdges = dyn_shmem_bytes/sizeof(IndexT) - (n_tileNodes+1);
+
+  extern __shared__ IndexT shMem[];
+  IndexT* shMemRows = shMem;                        // n_tileNodes + 1 elements
+  IndexT* shMemCols = &shMemRows[n_tileNodes+1];    // n_tileEdges elements
+  IndexT* shMemWorkspace = &shMemCols[n_tileEdges]; // n_tileNodes + n_tileEdges + 1 elements
+  
+  Partition2ShMem(shMemRows, shMemCols, row_ptr, col_ptr, tile_boundaries);
+  typedef cub::BlockReduce<int, THREADS, RED_ALGO> BlockReduceT;
+
+  __shared__ typename BlockReduceT::TempStorage temp_storage;
+
+
+  for (int k = 0; k < hash_params.len; ++k) {
+    Counters total1, max1;
+    Counters total2, max2;
+    D1CollisionsLocal(shMemRows, shMemCols, tile_boundaries, hash_params.val[k],
+                      total1, max1);
+    D2CollisionsLocal(shMemRows, shMemCols, shMemWorkspace, tile_boundaries,
+                      hash_params.val[k], total2, max2);
+    for (int i = 0; i < num_bit_widths; ++i) {
+      int l1_sum1 = BlockReduceT(temp_storage)
+                       .Reduce(total1.m[i], cub::Sum{}, n_tileNodes);
+      cg::this_thread_block().sync();
+      int l1_max1 = BlockReduceT(temp_storage)
+                       .Reduce(max1.m[i], cub::Max{}, n_tileNodes);
+      cg::this_thread_block().sync();
+      int l1_sum2 = BlockReduceT(temp_storage)
+                       .Reduce(total2.m[i], cub::Sum{}, n_tileNodes);
+      cg::this_thread_block().sync();
+      int l1_max2 = BlockReduceT(temp_storage)
+                       .Reduce(max2.m[i], cub::Max{}, n_tileNodes);
       cg::this_thread_block().sync();
 
       // soa has an int array for each hash function.
       // In this array all block reduce results of first counter are stored
       // contiguous followed by the reduce results of the next counter ...
       if(threadIdx.x == 0){
-        soa_total->m[k][partNr + i * gridDim.x] = l1_sum;
-        soa_max->m[k][partNr + i * gridDim.x] = l1_max;
+        soa_total1->m[k][partNr + i * gridDim.x] = l1_sum1;
+        soa_max1->m[k][partNr + i * gridDim.x] = l1_max1;
+        soa_total2->m[k][partNr + i * gridDim.x] = l1_sum2;
+        soa_max2->m[k][partNr + i * gridDim.x] = l1_max2;
       }
     }
   }
 }
+
+
+
