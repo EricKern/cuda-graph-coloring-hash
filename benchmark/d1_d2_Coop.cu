fb5fefbec7b8a636e7ad9eb0405f54f32cad2573
Eric Kern <k.eric0814@gmail.com>
2023-02-28 18:50:11 +0100

    Add remaining benchmarks

--- benchmark/d1_d2_Coop.cu
+++ benchmark/d1_d2_Coop.cu
@@ -3,18 +3,18 @@
 #include "defines.hpp"
 #include "setup.cuh"
 #include "coloring.cuh"
+#include "coop_launch.cuh"
 #include "cli_bench.cu"
 #include "mat_loader.hpp"
 #include "bench_util.cuh"
 
 #include "d1_opt_kernel.cuh"  // for dist1 struct reduction
 #include "d2_opt_kernel.cuh"
-#include "V2/WarpHash.cuh"
 
 using namespace apa22_coloring;
 
-using BLOCKS_SM = nvbench::enum_type_list<1, 2>;
-using THREADS_SM = nvbench::enum_type_list<384, 512, 640, 768, 896, 1024>;
+using BLOCKS_SM = nvbench::enum_type_list<1, 2, 4>;
+using THREADS_SM = nvbench::enum_type_list<256, 384, 512, 640, 1024>;
 // using BLOCKS_SM = nvbench::enum_type_list<1>;
 // using THREADS_SM = nvbench::enum_type_list<1024>;
 static constexpr const char* SM_ShMem_key = "Smem_SM";
@@ -52,47 +52,6 @@ void Dist1(nvbench::state& state,
         gpu_setup.blocks_max1,
         gpu_setup.d_total1,
         gpu_setup.d_max1);
-  });
-}
-
-template <int MAX_THREADS_SM, int BLK_SM>
-void Dist2(nvbench::state& state,
-                nvbench::type_list<nvbench::enum_type<MAX_THREADS_SM>,
-                                   nvbench::enum_type<BLK_SM>>) {
-  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
-  auto kernel = coloring2Kernel<THREADS, BLK_SM, int>;
-
-  MatLoader& mat_l = MatLoader::getInstance();
-  Tiling tiling(D2, BLK_SM,
-                mat_l.row_ptr,
-                mat_l.m_rows,
-                reinterpret_cast<void*>(kernel),
-                state.get_int64(SM_ShMem_key));
-  GPUSetupD2 gpu_setup(mat_l.row_ptr,
-                       mat_l.col_ptr,
-                       tiling.tile_boundaries.get(),
-                       tiling.n_tiles);
-
-  size_t shMem_bytes = tiling.tile_target_mem;
-  dim3 gridSize(tiling.n_tiles);
-  dim3 blockSize(THREADS);
-
-  add_MatInfo(state);
-  add_IOInfo(state, tiling.n_tiles);
-
-  state.exec([&](nvbench::launch& launch) {
-    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
-            gpu_setup.d_row_ptr,
-            gpu_setup.d_col_ptr,
-            gpu_setup.d_tile_boundaries,
-            gpu_setup.blocks_total1,
-            gpu_setup.blocks_total2,
-            gpu_setup.blocks_max1,
-            gpu_setup.blocks_max2,
-            gpu_setup.d_total1,
-            gpu_setup.d_max1,
-            gpu_setup.d_total2,
-            gpu_setup.d_max2);
   });
 }
 
@@ -137,54 +96,56 @@ void Dist2SmallSnet(nvbench::state& state,
   });
 }
 
+// =================================
+// Naive Coop port
+// =================================
+
 template <int MAX_THREADS_SM, int BLK_SM>
-void Dist2ThrustSrt(nvbench::state& state,
+void Dist1CoopBase(nvbench::state& state,
                 nvbench::type_list<nvbench::enum_type<MAX_THREADS_SM>,
                                    nvbench::enum_type<BLK_SM>>) {
   constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
-  auto kernel = coloring2KernelThrust<THREADS, BLK_SM, int>;
+  auto kernel = coloring1coopBase<THREADS,BLK_SM, int>;
 
   MatLoader& mat_l = MatLoader::getInstance();
-  Tiling tiling(D2, BLK_SM,
-                mat_l.row_ptr,
-                mat_l.m_rows,
+  Tiling tiling(D1, BLK_SM, mat_l.row_ptr, mat_l.m_rows,
                 reinterpret_cast<void*>(kernel),
                 state.get_int64(SM_ShMem_key));
-  GPUSetupD2 gpu_setup(mat_l.row_ptr,
-                       mat_l.col_ptr,
-                       tiling.tile_boundaries.get(),
-                       tiling.n_tiles);
+  GPUSetupD1 gpu_setup(mat_l.row_ptr, mat_l.col_ptr,
+                       tiling.tile_boundaries.get(), tiling.n_tiles);
 
   size_t shMem_bytes = tiling.tile_target_mem;
-  dim3 gridSize(tiling.n_tiles);
   dim3 blockSize(THREADS);
+  int coop_blocks = get_coop_grid_size(reinterpret_cast<void*>(kernel),
+                                       THREADS,
+                                       shMem_bytes);
+  dim3 gridSize(coop_blocks);
 
   add_MatInfo(state);
   add_IOInfo(state, tiling.n_tiles);
 
+  void *kernelArgs[] = {(void *)&gpu_setup.d_row_ptr,
+                        (void *)&gpu_setup.d_col_ptr,
+                        (void *)&gpu_setup.d_tile_boundaries,
+                        (void *)&tiling.n_tiles,
+                        (void *)&gpu_setup.blocks_total1,
+                        (void *)&gpu_setup.blocks_max1,
+                        (void *)&gpu_setup.d_total1,
+                        (void *)&gpu_setup.d_max1
+                        };
+
   state.exec([&](nvbench::launch& launch) {
-    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
-            gpu_setup.d_row_ptr,
-            gpu_setup.d_col_ptr,
-            gpu_setup.d_tile_boundaries,
-            gpu_setup.blocks_total1,
-            gpu_setup.blocks_total2,
-            gpu_setup.blocks_max1,
-            gpu_setup.blocks_max2,
-            gpu_setup.d_total1,
-            gpu_setup.d_max1,
-            gpu_setup.d_total2,
-            gpu_setup.d_max2);
+    cudaLaunchCooperativeKernel((void *)kernel, gridSize,
+                              blockSize, kernelArgs, shMem_bytes, NULL);
   });
 }
 
-
 template <int MAX_THREADS_SM, int BLK_SM>
-void Dist2Banks(nvbench::state& state,
+void Dist2CoopBase(nvbench::state& state,
                 nvbench::type_list<nvbench::enum_type<MAX_THREADS_SM>,
                                    nvbench::enum_type<BLK_SM>>) {
   constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
-  auto kernel = coloring2KernelBank<THREADS, BLK_SM, int>;
+  auto kernel = coloring2coopBase<THREADS,BLK_SM, int>;
 
   MatLoader& mat_l = MatLoader::getInstance();
   Tiling tiling(D2_SortNet, BLK_SM,
@@ -198,113 +159,147 @@ void Dist2Banks(nvbench::state& state,
                        tiling.n_tiles);
 
   size_t shMem_bytes = tiling.tile_target_mem;
-  dim3 gridSize(tiling.n_tiles);
   dim3 blockSize(THREADS);
+  int coop_blocks = get_coop_grid_size(reinterpret_cast<void*>(kernel),
+                                       THREADS,
+                                       shMem_bytes);
+  dim3 gridSize(coop_blocks);
 
   add_MatInfo(state);
   add_IOInfo(state, tiling.n_tiles);
 
+  void *kernelArgs[] = {(void *)&gpu_setup.d_row_ptr,
+                        (void *)&gpu_setup.d_col_ptr,
+                        (void *)&gpu_setup.d_tile_boundaries,
+                        (void *)&tiling.n_tiles,
+                        (void *)&tiling.max_node_degree,
+                        (void *)&gpu_setup.blocks_total1,
+                        (void *)&gpu_setup.blocks_total2,
+                        (void *)&gpu_setup.blocks_max1,
+                        (void *)&gpu_setup.blocks_max2,
+                        (void *)&gpu_setup.d_total1,
+                        (void *)&gpu_setup.d_max1,
+                        (void *)&gpu_setup.d_total2,
+                        (void *)&gpu_setup.d_max2
+                        };
+
   state.exec([&](nvbench::launch& launch) {
-    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
-            gpu_setup.d_row_ptr,
-            gpu_setup.d_col_ptr,
-            gpu_setup.d_tile_boundaries,
-            tiling.max_node_degree,
-            gpu_setup.blocks_total1,
-            gpu_setup.blocks_total2,
-            gpu_setup.blocks_max1,
-            gpu_setup.blocks_max2,
-            gpu_setup.d_total1,
-            gpu_setup.d_max1,
-            gpu_setup.d_total2,
-            gpu_setup.d_max2);
+    cudaLaunchCooperativeKernel((void *)kernel, gridSize,
+                              blockSize, kernelArgs, shMem_bytes, NULL);
   });
 }
 
+// =================================
+// Less global writes
+// =================================
 
 template <int MAX_THREADS_SM, int BLK_SM>
-void D2WarpMapping(nvbench::state& state,
+void Dist1Coop(nvbench::state& state,
                 nvbench::type_list<nvbench::enum_type<MAX_THREADS_SM>,
                                    nvbench::enum_type<BLK_SM>>) {
   constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
-  auto kernel = D2warp<THREADS, BLK_SM, 16, 3, int, char, 8, 3, int>;
+  auto kernel = coloring1coop<THREADS,BLK_SM, int>;
 
   MatLoader& mat_l = MatLoader::getInstance();
-  Tiling tiling(D2_Warp, BLK_SM, mat_l.row_ptr, mat_l.m_rows,
+  Tiling tiling(D1, BLK_SM, mat_l.row_ptr, mat_l.m_rows,
                 reinterpret_cast<void*>(kernel),
                 state.get_int64(SM_ShMem_key));
-  GPUSetupD2 gpu_setup(mat_l.row_ptr, mat_l.col_ptr,
+  GPUSetupD1 gpu_setup(mat_l.row_ptr, mat_l.col_ptr,
                        tiling.tile_boundaries.get(), tiling.n_tiles);
 
   size_t shMem_bytes = tiling.tile_target_mem;
-  dim3 gridSize(tiling.n_tiles);
   dim3 blockSize(THREADS);
+  int coop_blocks = get_coop_grid_size(reinterpret_cast<void*>(kernel),
+                                       THREADS,
+                                       shMem_bytes);
+  dim3 gridSize(coop_blocks);
 
   add_MatInfo(state);
-  add_IOInfo(state, tiling.n_tiles);
+  add_IOInfo(state, gridSize.x);
+
+  void *kernelArgs[] = {(void *)&gpu_setup.d_row_ptr,
+                        (void *)&gpu_setup.d_col_ptr,
+                        (void *)&gpu_setup.d_tile_boundaries,
+                        (void *)&tiling.n_tiles,
+                        (void *)&gpu_setup.blocks_total1,
+                        (void *)&gpu_setup.blocks_max1,
+                        (void *)&gpu_setup.d_total1,
+                        (void *)&gpu_setup.d_max1
+                        };
 
   state.exec([&](nvbench::launch& launch) {
-    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
-        gpu_setup.d_row_ptr, gpu_setup.d_col_ptr, gpu_setup.d_tile_boundaries,
-        tiling.max_node_degree,
-        gpu_setup.blocks_total2, gpu_setup.blocks_max2, gpu_setup.d_total2,
-        gpu_setup.d_max2);
+    cudaLaunchCooperativeKernel((void *)kernel, gridSize,
+                              blockSize, kernelArgs, shMem_bytes, NULL);
   });
 }
 
 template <int MAX_THREADS_SM, int BLK_SM>
-void D2Warp_Small_Mapping(nvbench::state& state,
+void Dist2Coop(nvbench::state& state,
                 nvbench::type_list<nvbench::enum_type<MAX_THREADS_SM>,
                                    nvbench::enum_type<BLK_SM>>) {
   constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
-  auto kernel = D2warp_Conflicts<THREADS, BLK_SM, 16, 3, int, char, 8, 3, int>;
+  auto kernel = coloring2coop<THREADS,BLK_SM, int>;
 
   MatLoader& mat_l = MatLoader::getInstance();
-  Tiling tiling(D2_Warp_small, BLK_SM, mat_l.row_ptr, mat_l.m_rows,
+  Tiling tiling(D2_SortNet, BLK_SM,
+                mat_l.row_ptr,
+                mat_l.m_rows,
                 reinterpret_cast<void*>(kernel),
                 state.get_int64(SM_ShMem_key));
-  GPUSetupD2 gpu_setup(mat_l.row_ptr, mat_l.col_ptr,
-                       tiling.tile_boundaries.get(), tiling.n_tiles);
+  GPUSetupD2 gpu_setup(mat_l.row_ptr,
+                       mat_l.col_ptr,
+                       tiling.tile_boundaries.get(),
+                       tiling.n_tiles);
 
   size_t shMem_bytes = tiling.tile_target_mem;
-  dim3 gridSize(tiling.n_tiles);
   dim3 blockSize(THREADS);
+  int coop_blocks = get_coop_grid_size(reinterpret_cast<void*>(kernel),
+                                       THREADS,
+                                       shMem_bytes);
+  dim3 gridSize(coop_blocks);
 
   add_MatInfo(state);
-  add_IOInfo(state, tiling.n_tiles);
+  add_IOInfo(state, gridSize.x);
+
+  void *kernelArgs[] = {(void *)&gpu_setup.d_row_ptr,
+                        (void *)&gpu_setup.d_col_ptr,
+                        (void *)&gpu_setup.d_tile_boundaries,
+                        (void *)&tiling.n_tiles,
+                        (void *)&tiling.max_node_degree,
+                        (void *)&gpu_setup.blocks_total1,
+                        (void *)&gpu_setup.blocks_total2,
+                        (void *)&gpu_setup.blocks_max1,
+                        (void *)&gpu_setup.blocks_max2,
+                        (void *)&gpu_setup.d_total1,
+                        (void *)&gpu_setup.d_max1,
+                        (void *)&gpu_setup.d_total2,
+                        (void *)&gpu_setup.d_max2
+                        };
 
   state.exec([&](nvbench::launch& launch) {
-    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
-        gpu_setup.d_row_ptr, gpu_setup.d_col_ptr, gpu_setup.d_tile_boundaries,
-        gpu_setup.blocks_total2, gpu_setup.blocks_max2, gpu_setup.d_total2,
-        gpu_setup.d_max2);
+    cudaLaunchCooperativeKernel((void *)kernel, gridSize,
+                              blockSize, kernelArgs, shMem_bytes, NULL);
   });
 }
 
-// NVBENCH_BENCH_TYPES(Dist1, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
-//     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
-//     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
-
-// NVBENCH_BENCH_TYPES(Dist2ThrustSrt, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
-//     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
-//     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
-    
-// NVBENCH_BENCH_TYPES(Dist2Banks, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
-//     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
-//     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
-
+NVBENCH_BENCH_TYPES(Dist1, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
+    .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
+    .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
 NVBENCH_BENCH_TYPES(Dist2SmallSnet, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
 
-// NVBENCH_BENCH_TYPES(Dist2, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
-//     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
-//     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
-    
-NVBENCH_BENCH_TYPES(D2WarpMapping, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
+NVBENCH_BENCH_TYPES(Dist1CoopBase, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
+    .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
+    .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
+NVBENCH_BENCH_TYPES(Dist2CoopBase, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
 
-NVBENCH_BENCH_TYPES(D2Warp_Small_Mapping, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
+NVBENCH_BENCH_TYPES(Dist1Coop, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
+NVBENCH_BENCH_TYPES(Dist2Coop, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
+    .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
+    .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
+    
--- benchmark/d1_opt_kernels.cu
+++ benchmark/d1_opt_kernels.cu
@@ -5,6 +5,7 @@
 #include "setup.cuh"
 #include "mat_loader.hpp"
 #include "d1_opt_kernel.cuh"
+#include "V2/WarpHash.cuh"
 
 using BLOCKS_SM = nvbench::enum_type_list<1, 2>;
 using THREADS_SM = nvbench::enum_type_list<256, 384, 512, 640, 768, 1024>;
@@ -166,6 +167,35 @@ void D1StructBreak(nvbench::state& state,
   });
 }
 
+template <int MAX_THREADS_SM, int BLK_SM>
+void D1WarpMapping(nvbench::state& state,
+                nvbench::type_list<nvbench::enum_type<MAX_THREADS_SM>,
+                                   nvbench::enum_type<BLK_SM>>) {
+  constexpr int THREADS = MAX_THREADS_SM / BLK_SM;
+  auto kernel = D1warp<THREADS, BLK_SM, 16, 3, int, char, 8, 3, int>;
+
+  MatLoader& mat_l = MatLoader::getInstance();
+  Tiling tiling(D1_Warp, BLK_SM, mat_l.row_ptr, mat_l.m_rows,
+                reinterpret_cast<void*>(kernel),
+                state.get_int64(SM_ShMem_key));
+  GPUSetupD1 gpu_setup(mat_l.row_ptr, mat_l.col_ptr,
+                       tiling.tile_boundaries.get(), tiling.n_tiles);
+
+  size_t shMem_bytes = tiling.tile_target_mem;
+  dim3 gridSize(tiling.n_tiles);
+  dim3 blockSize(THREADS);
+
+  add_MatInfo(state);
+  add_IOInfo(state, tiling.n_tiles);
+
+  state.exec([&](nvbench::launch& launch) {
+    kernel<<<gridSize, blockSize, shMem_bytes, launch.get_stream()>>>(
+        gpu_setup.d_row_ptr, gpu_setup.d_col_ptr, gpu_setup.d_tile_boundaries,
+        gpu_setup.blocks_total1, gpu_setup.blocks_max1, gpu_setup.d_total1,
+        gpu_setup.d_max1);
+  });
+}
+
     
 NVBENCH_BENCH_TYPES(D1Naive, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
@@ -184,5 +214,9 @@ NVBENCH_BENCH_TYPES(D1Struct, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
 
 NVBENCH_BENCH_TYPES(D1StructBreak, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
+    .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
+    .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
+
+NVBENCH_BENCH_TYPES(D1WarpMapping, NVBENCH_TYPE_AXES(THREADS_SM, BLOCKS_SM))
     .set_type_axes_names({Threads_SM_key, Blocks_SM_key})
     .add_int64_axis(SM_ShMem_key, SM_ShMem_range);
